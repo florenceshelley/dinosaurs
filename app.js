c9ea5b43bcbacd5c87eeda50f95d9ebd53648d38
Florence Shelley <smc.florence@gmail.com>
2022-07-20 21:42:44 -0400

    Change UI

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
+'use strict';
+function Creature() {
+	// Dino
+	// species
+	// image
+	// fact
 
-    // Create Dino Constructor
+	// Human
+	// name
+	// image
 
+	// Bird is a subclass of Dino
+	// species
+	// image
+	// fact
+}
 
-    // Create Dino Objects
-
-
-    // Create Human Object
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Objects
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Human Object
 
-    // Remove form from screen
+// Use IIFE to get human data from form
+
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
